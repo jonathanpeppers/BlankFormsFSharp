bbd726358ca7a1b90a05e59dee4c0876d7e2fc08
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-04-17 11:57:15 -0500

    File->New F# project

---
+++ BlankFormsFSharp/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace BlankFormsFSharp
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("BlankFormsFSharp")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
---
+++ Droid/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿namespace BlankFormsFSharp.Droid
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+// the name of the type here needs to match the name inside the ResourceDesigner attribute
+type Resources = BlankFormsFSharp.Droid.Resource
+
+[<assembly:Android.Runtime.ResourceDesigner("BlankFormsFSharp.Droid.Resources", IsApplication = true)>]
+[<assembly:AssemblyTitle("BlankFormsFSharp.Droid")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
